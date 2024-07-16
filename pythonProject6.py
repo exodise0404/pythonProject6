69e75d6239fe5254c71cb9f4b4dee67182fa3d0d
exodise0404 <exodise0404@gmail.com>
2024-07-16 21:17:01 +0800

    Add files via upload

---
+++ pythonProject6.py
@@ -0,0 +1,6 @@
+immutable_var = 1,2,True,'кортеж'
+print(immutable_var)
+#immutable_var = [2] = False #кортеж не поддерживает обращение по элементам
+mutable_list =[1,2,True,'кортеж']
+mutable_list [2] = False
+print(mutable_list)
